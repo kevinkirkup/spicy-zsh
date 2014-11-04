95d974747bff884cc1f57fbf85896a04d2bf7bed
Kevin S Kirkup <kevin.s.kirkup@me.com>
2014-11-03 20:25:06 -0500

    Add script to perform URL Encoding with python

--- home/.zsh/python.sh
+++ home/.zsh/python.sh
@@ -4,3 +4,11 @@
 
 alias servethis='python -m SimpleHTTPServer 8080'
 alias ipn='ipython notebook --pylab inline'
+
+# URL encode a string
+#
+# Example:
+# $ urlencode 'q werty=/;'
+# q+werty%3D%2F%3B
+#
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
