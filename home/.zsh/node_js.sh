569b003820a8f94596427b81b522ca028f234034
Kevin S Kirkup <kevin.s.kirkup@me.com>
2015-05-23 09:34:26 -0400

    Update Nodejs path

--- home/.zsh/node_js.sh
+++ home/.zsh/node_js.sh
@@ -1,7 +1,4 @@
 #!/usr/local/env zsh
 
 # Add the path to the node.js libraries
-export NODE_PATH="/usr/local/lib/node"
-
-# Add the path npm binaries to the path
-export PATH="${PATH}:/usr/local/share/npm/bin"
+export NODE_PATH="/usr/local/lib/node_modules"
