b05d5f848eb4c183c98b33530597b84550bb41cc
Kevin S Kirkup <kevin.s.kirkup@me.com>
2015-05-23 09:36:38 -0400

    Add xcodebuild command to display build settings

--- home/.zsh/mac/xcode.sh
+++ home/.zsh/mac/xcode.sh
@@ -6,3 +6,17 @@ function localize_project() {
   find . -name \*.m | xargs genstrings -o en.lproj
   find . -name \*.xib | xargs -t -I '{}' ibtool --generate-strings '{}'.strings '{}'
 }
+
+############################################################
+# Show workspace settings
+function build_settings() {
+
+  local PROJECT_NAME=$1
+  local TARGET=$2
+
+  if [ -z "$TARGET" ]; then
+    TARGET=$PROJECT_NAME
+  fi
+
+  xcodebuild -project ${PROJECT_NAME}.xcodeproj -target "${TARGET}" -showBuildSettings
+}
