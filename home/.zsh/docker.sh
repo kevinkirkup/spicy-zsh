206599c4d98d1e40a9fbf42854db1b627f707490
Kevin S Kirkup <kevin.s.kirkup@me.com>
2016-01-19 22:44:52 -0500

    Add docker alias's and commands

--- home/.zsh/docker.sh
+++ home/.zsh/docker.sh
@@ -2,8 +2,9 @@
 
 # Docker related alias' and functions
 
-alias dc='docker rm `docker ps --no-trunc -aq`'
 alias dls='docker ps -a'
 alias di='docker images'
-alias drm='docker rmi'
 
+alias dci='docker rmi $(docker images -f dangling=true -q)
+alias dcc='docker rm $(docker ps -qa)
+alias din='docker inspect'
---
+++ home/.zsh/mac/docker.sh
@@ -0,0 +1,2 @@
+# Docker configuration and environment
+eval $(/usr/local/bin/docker-machine env default)
