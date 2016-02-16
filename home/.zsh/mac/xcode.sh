8712562a528de83ea1989b3035a5f0f04319f2bf
Kevin S Kirkup <kevin.s.kirkup@me.com>
2016-02-16 07:57:25 -0500

    Add zsh function to build fat xcode frameworks

--- home/.zsh/mac/xcode.sh
+++ home/.zsh/mac/xcode.sh
@@ -20,3 +20,37 @@ function build_settings() {
 
   xcodebuild -project ${PROJECT_NAME}.xcodeproj -target "${TARGET}" -showBuildSettings
 }
+
+############################################################
+# Show workspace settings
+function build_fat_framework() {
+
+  local FRAMEWORK_NAME=$1
+
+  # Combine the target binaries in to a single object
+  lipo -output ${FRAMEWORK_NAME} -create "Debug-iphonesimulator/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "Debug-iphoneos/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}"
+
+  # Re-create the frameworks
+  cp -R Debug-iphonesimulator/${FRAMEWORK_NAME}.framework .
+  mv ${FRAMEWORK_NAME} ${FRAMEWORK_NAME}.framework/
+}
+
+############################################################
+# Show workspace settings
+function extract_arch_framework() {
+
+  local FRAMEWORK_EXECUTABLE_PATH=$1
+  local ARCHS=$2
+
+  for ARCH in $ARCHS
+  do
+      echo "Extracting $ARCH..."
+      # Combine the target binaries in to a single object
+      lipo -extract "$ARCH" "$FRAMEWORK_EXECUTABLE_PATH" -o "$FRAMEWORK_EXECUTABLE_PATH-$ARCH"
+      EXTRACTED_ARCHS+=("$FRAMEWORK_EXECUTABLE_PATH-$ARCH")
+  done
+
+  echo "Merging binaries..."
+  lipo -o "$FRAMEWORK_EXECUTABLE_PATH-merged" -create "${EXTRACTED_ARCHS[@]}"
+  rm "${EXTRACTED_ARCHS[@]}"
+}
