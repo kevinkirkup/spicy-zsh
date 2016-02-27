0f4cd4568b2fa23661e9760394db91b2ec81b967
Kevin S Kirkup <kevin.kirkup@cisco.com>
2016-02-27 10:09:26 -0500

    Add Rust shell functions and alias'

--- home/.zsh/docker.sh
+++ home/.zsh/docker.sh
@@ -6,3 +6,8 @@
 #alias dci='docker rmi $(docker images -f dangling=true -q)'
 #alias dcc='docker rm $(docker ps -qa)'
 #alias din='docker inspect'
+
+# Keep docker open so we can attach a shell
+function docker_delay() {
+  docker run -d $1 /bin/sh -c "while true; do echo Docker; sleep 1; done"
+}
