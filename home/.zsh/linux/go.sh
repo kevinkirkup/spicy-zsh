ccfcb2d28112423c38355fe7d13b941b2c53b649
Kevin S Kirkup <kevin.kirkup@cisco.com>
2016-08-01 15:25:56 -0400

    Add Go configurations

---
+++ home/.zsh/linux/go.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+# --------------------------------------------------
+# Linux Go configurations
+# --------------------------------------------------
+export PATH=$PATH:/usr/local/go/bin
+
