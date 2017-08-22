698102628dea03a88ed36c899ab8943e9312e8df
Kevin S Kirkup <kevin.kirkup@cisco.com>
2017-08-22 17:46:34 -0400

    Add minikube environment configuration

---
+++ home/.zsh/k8.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+##################################################
+# Minikube configurations
+##################################################
+
+# minikube/docs/env_vars.md
+export MINIKUBE_WANTUPDATENOTIFICATION=1
+export MINIKUBE_REMINDERWAITPERIODINHOURS=24
+export MINIKUBE_WANTREPORTERROR=1
+export MINIKUBE_WANTREPORTERRORPROMPT=1
