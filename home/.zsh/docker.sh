6f12cad965a298d3f308beaa2cac72c9b0d1224a
Kevin S Kirkup <kevin.kirkup@cisco.com>
2017-10-19 19:14:25 -0400

    Add functions for docker

--- home/.zsh/docker.sh
+++ home/.zsh/docker.sh
@@ -7,6 +7,24 @@
 #alias dcc='docker rm $(docker ps -qa)'
 #alias din='docker inspect'
 
+
+# Function to launch a docker image in interactive mode
+# Remove the image when we are done
+function docker_interactive_remove() {
+
+  local IMAGE=$1
+  docker run --rm -it $IMAGE /bin/bash
+}
+alias dri='docker_interactive_remove'
+
+# Function to launch a docker image in interactive mode
+function docker_interactive_remove() {
+
+  local IMAGE=$1
+  docker run --rm -it $IMAGE /bin/bash
+}
+alias dri='docker_interactive_remove'
+
 # Keep docker open so we can attach a shell
 function docker_delay() {
   docker run -d $1 /bin/sh -c "while true; do echo Docker; sleep 1; done"
