6d93dd2b667d370877e54b492a77e414cbb57fe5
Kevin S Kirkup <kevin.kirkup@cisco.com>
2018-10-26 16:00:57 -0400

    Update the time/date in docker-machine VM
    When your laptop goes in to sleep mode, the time inside
    docker-machine VMs doesnt' progress. This causes files
    mounted inside the VM to be in the future. This script
    will correct the sync the VM to the host time/date.

--- home/.zsh/mac/docker.sh
+++ home/.zsh/mac/docker.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env zsh
+
 # Docker configuration and environment
 #eval $(/usr/local/bin/docker-machine env default)
+
+# Update the date/time on the vm
+update_docker_time() {
+  eval $(docker-machine env default)
+  docker run --privileged --rm alpine date -s "$(date -u "+%Y-%m-%d %H:%M:%S")"
+}
