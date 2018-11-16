eb4076b091dcd4f5a17d95d0af70d5eca37413a7
Kevin S Kirkup <kevin.kirkup@cisco.com>
2018-11-16 16:53:37 -0500

    Updates for docker

--- home/.zsh/docker.sh
+++ home/.zsh/docker.sh
@@ -29,3 +29,15 @@ alias dri='docker_interactive_remove'
 function docker_delay() {
   docker run -d $1 /bin/sh -c "while true; do echo Docker; sleep 1; done"
 }
+
+# Create a docker-machine VM using the xhyve driver
+function docker_machine_xhyve() {
+  docker-machine create default --driver xhyve --xhyve-memory-size 4096 --xhyve-cpu-count 4 --xhyve-experimental-nfs-share=true --xhyve-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.06.1-ce/boot2docker.iso
+
+  #docker-machine create default --driver xhyve --xhyve-memory-size 4096 --xhyve-cpu-count 4 --xhyve-experimental-nfs-share=true --engine-storage-driver devicemapper
+}
+
+# Create a docker-machine VM using the VirtualBox driver
+function docker_machine_vbox() {
+  docker-machine create default --driver virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 4 --virtualbox-ui-type "headless"
+}
