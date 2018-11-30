c2472321c2c12db6763f97bf4ab858205954f715
Kevin S Kirkup <kevin.kirkup@cisco.com>
2018-11-30 10:36:28 -0500

    git clone ansible-galaxy requirements

--- home/.zsh/git.sh
+++ home/.zsh/git.sh
@@ -13,3 +13,40 @@ function gen_tag() {
 
   echo $IMAGE_TAG
 }
+
+##################################################
+# Function to clone all role repos base on the
+# ansible-galaxy requirements files in the subtree
+##################################################
+function clone_repos() {
+
+  # --------------------------------------------------
+  # Find the Repos in the available bundles
+  # --------------------------------------------------
+  for repo in `cat $(find . -iname "requirements.yml"|grep "/bundle/requirements.yml") | grep src | awk -F" " '{ print $3 }' | sort| uniq`; do
+
+    echo Updated Repo: $repo
+
+    # --------------------------------------------------
+    # If the Role Repo already exists, just update it to the latest
+    # --------------------------------------------------
+    if [[ -d roles/$(basename "$repo") ]]; then
+
+      local BASE=$(pwd)
+
+      cd roles/$(basename "$repo")
+      git fetch
+      git pull
+
+      cd $BASE
+
+    # --------------------------------------------------
+    # Clone the Repo
+    # --------------------------------------------------
+    else
+      git clone $repo roles/$(basename "$repo")
+    fi
+
+  done
+
+}
