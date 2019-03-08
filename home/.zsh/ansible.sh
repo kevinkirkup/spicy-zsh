7723cce8d7fd70e28330d9b624e7fa8aacfcadbe
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-03-08 09:19:10 -0500

    Add function to validate Ansible roles using ansible-review

--- home/.zsh/ansible.sh
+++ home/.zsh/ansible.sh
@@ -4,3 +4,9 @@ export ANSIBLE_COW_SELECTION=owl
 
 # Provision the entire site
 alias ap='ansible-playbook site.yml --ask-sudo'
+
+# Function to validate ansible roles using the python package
+# ansible-review. NOTE! You'll need install ansible-review with pip
+function validate_ansible_roles() {
+  find roles -type f | xargs ansible-review -c ansible-review/config.ini
+}
