a4f82ec55efdf6c5d77418ac90d2b5a064328d74
Kevin S Kirkup <kevin.kirkup@pureport.com>
2019-03-08 09:19:46 -0500

    Update functions to create docker-machine image with hyperkit

--- home/.zsh/docker.sh
+++ home/.zsh/docker.sh
@@ -30,14 +30,36 @@ function docker_delay() {
   docker run -d $1 /bin/sh -c "while true; do echo Docker; sleep 1; done"
 }
 
-# Create a docker-machine VM using the xhyve driver
-function docker_machine_xhyve() {
-  docker-machine create default --driver xhyve --xhyve-memory-size 4096 --xhyve-cpu-count 4 --xhyve-experimental-nfs-share=true --xhyve-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.06.1-ce/boot2docker.iso
+##################################################
+# Docker Machine
+##################################################
 
-  #docker-machine create default --driver xhyve --xhyve-memory-size 4096 --xhyve-cpu-count 4 --xhyve-experimental-nfs-share=true --engine-storage-driver devicemapper
+# Note! Sometimes you may need to work around a bug in boot2docker by using a different version
+# You can do that by adding the --<driver>-boot2docker-url parameter and specify the URL
+# to the boot2docker release
+# Ex.
+#
+# docker-machine create default \
+#   --driver virtualbox \
+#   --virtualbox-memory 4096 \
+#   --virtualbox-cpu-count 4 \
+#   --virtualbox-ui-type "headless" \
+#   --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.09.1-rc1/boot2docker.iso
+
+# --------------------------------------------------
+# Create a docker-machine VM using the xhyve driver
+# --------------------------------------------------
+function docker_machine_xhyve() {
+  docker-machine create default --driver xhyve --xhyve-memory-size 4096 --xhyve-cpu-count 4 --xhyve-experimental-nfs-share=true --engine-storage-driver devicemapper
 }
 
+function docker_machine_hyperkit() {
+  docker-machine create default --driver hyperkit --hyperkit-memory-size 4096 --hyperkit-cpu-count 4 --hyperkit-experimental-nfs-share=true --engine-storage-driver devicemapper
+}
+
+# --------------------------------------------------
 # Create a docker-machine VM using the VirtualBox driver
+# --------------------------------------------------
 function docker_machine_vbox() {
   docker-machine create default --driver virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 4 --virtualbox-ui-type "headless"
 }
