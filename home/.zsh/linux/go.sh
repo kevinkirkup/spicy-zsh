bc8dfbadb0dfce73f9527110f8e90c76a0a18c7f
Kevin S Kirkup <kevin.kirkup@pureport.com>
2020-09-18 11:40:41 -0400

    Separate golang configuration for linux and Mac

--- home/.zsh/linux/go.sh
+++ home/.zsh/linux/go.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env zsh
-# --------------------------------------------------
-# Linux Go configurations
-# --------------------------------------------------
-export PATH=$PATH:/usr/local/go/bin
+#!/bin/sh
 
+# Set the go base directory
+export GOPATH=${HOME}/go
+
+# Add the go tools to PATH
+export PATH=${PATH}:${GOPATH}/bin
--- home/.zsh/mac/go.sh
+++ home/.zsh/mac/go.sh
@@ -2,6 +2,7 @@
 
 # Set the go base directory
 export GOPATH=${HOME}/go
+export GOROOT=/usr/local/opt/go/libexec
 
 # Add the go tools to PATH
 export PATH=${PATH}:${GOPATH}/bin
