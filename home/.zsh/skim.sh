e4991e349042781256b7778fd238c221da19e899
Kevin S Kirkup <kevin.kirkup@pureport.com>
2020-09-18 11:44:16 -0400

    Refine skim configuration

--- home/.zsh/skim.sh
+++ home/.zsh/skim.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 #export SKIM_DEFAULT_COMMAND=
 export SKIM_DEFAULT_OPTIONS='--ansi --regex --delimiter ":" --prompt ">>> " --cmd-prompt "==> "'
 
 # Function to do fuzzy search for filename
-function ski() {
+function skim_file_search() {
   mvim --remote-tab-silent $(sk -m)
 }
 
 # Function to do fuzzy search of file contents
-function skag() {
+function skim_file_content_search() {
   mvim --remote-tab-silent $(sk -i -c 'rg --color=always --line-number "{}"' | awk -F: '{print " +" $2 " " $1 }')
 }
+
+alias ski='skim_file_search'
+alias skag='skim_file_content_search'
