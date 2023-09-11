34b3e53f48cfda9c22d1589c2a7225c0ba6f6728
Kevin S Kirkup <kevin.s.kirkup@me.com>
2023-09-11 13:48:22 -0400

    Add Llama2 configuration

---
+++ home/.zsh/mac/llama2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+# Llama2 specific configurations
+
+##################################################
+# Llama 2
+##################################################
+# Build it. `LLAMA_METAL=1` allows the computation to be executed on the GPU
+export LLAMA_METAL=1
