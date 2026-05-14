1805791818622dcaa0fad1a7b2d74d0ba86311da
Kevin S Kirkup <kevin.kirkup@gmail.com>
2026-05-13 20:11:05 -0400

    feat: add path.sh consolidating platform PATH setup

---
+++ home/.zsh/path.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+if [[ $platform == mac ]]; then
+  if [[ $(uname -m) == "arm64" ]]; then
+    export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH
+  fi
+  export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:$PATH
+  export PATH=$PATH:/Developer/usr/bin
+  export PATH=$PATH:${HOME}/Library/Python/2.7/bin
+  export PATH=$PATH:${HOME}/Library/Python/3.10/bin
+  export PATH=/opt/homebrew/opt/postgresql@17/bin:$PATH
+  PATH="/Users/kevinkirkup/perl5/bin${PATH:+:${PATH}}"; export PATH
+else
+  export PATH=$PATH:/opt/games
+  PATH="/home/kevinkirkup/perl5/bin${PATH:+:${PATH}}"; export PATH
+fi
+
+export PATH=$PATH:$HOME/shared/bin
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/.local/bin
