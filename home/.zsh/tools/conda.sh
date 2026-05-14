0b83edf5917d9eb9edc4c3ce4f7686753181ef48
Kevin S Kirkup <kevin.kirkup@gmail.com>
2026-05-13 20:24:49 -0400

    fix: use $HOME in conda.sh, remove duplicate docker function

--- home/.zsh/tools/conda.sh
+++ home/.zsh/tools/conda.sh
@@ -13,14 +13,14 @@ if [[ $platform == mac ]]; then
       fi
     fi
   else
-    __conda_setup="$('/Users/kevinkirkup/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
       eval "$__conda_setup"
     else
-      if [ -f "/Users/kevinkirkup/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/kevinkirkup/anaconda3/etc/profile.d/conda.sh"
+      if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
       else
-        export PATH="/Users/kevinkirkup/anaconda3/bin:$PATH"
+        export PATH="$HOME/anaconda3/bin:$PATH"
       fi
     fi
   fi
@@ -37,14 +37,14 @@ else
       fi
     fi
   else
-    __conda_setup="$('/home/kevinkirkup/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    __conda_setup="$('$HOME/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
     if [ $? -eq 0 ]; then
       eval "$__conda_setup"
     else
-      if [ -f "/home/kevinkirkup/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/kevinkirkup/anaconda3/etc/profile.d/conda.sh"
+      if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
       else
-        export PATH="/home/kevinkirkup/anaconda3/bin:$PATH"
+        export PATH="$HOME/anaconda3/bin:$PATH"
       fi
     fi
   fi
--- home/.zsh/tools/docker.sh
+++ home/.zsh/tools/docker.sh
@@ -17,14 +17,6 @@ function docker_interactive_remove() {
 }
 alias dri='docker_interactive_remove'
 
-# Function to launch a docker image in interactive mode
-function docker_interactive_remove() {
-
-  local IMAGE=$1
-  docker run --rm -it $IMAGE /bin/bash
-}
-alias dri='docker_interactive_remove'
-
 # Keep docker open so we can attach a shell
 function docker_delay() {
   docker run -d $1 /bin/sh -c "while true; do echo Docker; sleep 1; done"
